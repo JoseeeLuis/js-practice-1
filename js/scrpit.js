e99fafc38cf5362f7f1ac77b12d7f3ad28a0412a
JoseeeLuis <jose.aranda@gm2dev.com>
2023-08-08 12:21:50 -0300

    feat(script.js): add a variable to cuotas

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,9 +1,11 @@
 function calculateSalePrice(cost, discount) {
+    let cuotas= cost/12;
+
     return {
         discountMessage: `Descuento: el descuento es de ${discount}%
     Precio con descuento: $${cost - (cost * discount) / 100}`,
         installmentMessage: `Hasta 12 cuotas sin descuento de: $${
-        cost / 12
+        cuotas.toFixed(2)
         } por mes`,
     };
 }
@@ -17,7 +19,7 @@ function createProduct(itemNumber, itemName, itemCost) {
 }
 
 function addDescriptionAndSaleInfo(product, itemDescription) {
-  const saleInfo = calculateSalePrice(product.itemCost, 10); // Ejemplo de descuento del 10%
+    const saleInfo = calculateSalePrice(product.itemCost, 10); 
 
     product.itemDescription = itemDescription;
     product.discountInfo = saleInfo.discountMessage;
@@ -25,23 +27,23 @@ function addDescriptionAndSaleInfo(product, itemDescription) {
 
     return product;
 }
-let productos = [ `Mackbook`, `Iphone-14`,`apple-watch`]
-let productos2 = [`cookies`, `bread`, `milk`] 
+let items = [ `Mackbook`, `Iphone-14`,`apple-watch`]
+let cost = [ 5000, 2230,645]
+items.push(`Iphone-13`)
+items.push(`oculus`)
+cost.unshift(2000)
+cost.unshift(50000)
 
-const product1 = createProduct(1, "MacBook", 600);
+const product1 = createProduct(1, items[0], cost[0]);
 
 const productWithDescription1 = addDescriptionAndSaleInfo(product1,"Esta MacBook tiene 16 GB de RAM. Es la mas nueva del mercado tiene un pentium de ultima generacion ");
 
 console.log(productWithDescription1);
 
-const product2 = createProduct(2, "iPhone", 800);
+const product2 = createProduct(2, items[1], cost[1]);
 const productWithDescription2 = addDescriptionAndSaleInfo(product2,"Este iPhone tiene una cámara avanzada. la camara cuenta con 10px de gran angular y 1px de macro");
 console.log(productWithDescription2);
 
-productos.push(`Iphone-13`)
-productos.push(`oculus`)
-productos2.unshift(`coffe`)
-productos2.unshift(`suggar`)
+console.log(items)
+console.log(cost)   
 
-console.log(productos)
-console.log(productos2)
